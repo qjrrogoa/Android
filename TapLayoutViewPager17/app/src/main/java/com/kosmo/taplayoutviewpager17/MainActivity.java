d52efcfbe3e8b74c8c2d064d8f311ba420b29812
Daemin Park <qjrrogoa@naver.com>
2021-06-15 10:37:50 +0900

    Fragment

---
+++ TapLayoutViewPager17/app/src/main/java/com/kosmo/taplayoutviewpager17/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kosmo.taplayoutviewpager17;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
