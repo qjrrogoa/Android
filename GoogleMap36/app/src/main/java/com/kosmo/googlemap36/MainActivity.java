fae4466618dc666cb32b8c18d03da6a681c61b02
Daemin Park <qjrrogoa@naver.com>
2021-06-28 20:38:44 +0900

    GoogleMap36

---
+++ GoogleMap36/app/src/main/java/com/kosmo/googlemap36/MainActivity.java
@@ -0,0 +1,21 @@
+package com.kosmo.googlemap36;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//////////onCreate
+    public void moveLocationByLatLng(View view){
+
+    }
+    public void moveLocationByAddress(View view){
+
+    }
+}
